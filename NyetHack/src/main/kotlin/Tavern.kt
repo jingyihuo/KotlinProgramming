dfb4f03e3c6fae2a03e614b2c1134326bea348ec
jingyihuo <jingyihuo@hotmail>
2022-01-08 17:13:26 -0800

    Adds Strings

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,16 +1,44 @@
-import java.util.*
+const val TAVERN_NAME = "Taernyl's Folly"
 
 fun main() {
-    var beverage = readLine()
-    beverage = null
-    beverage?.let {
-        beverage = it.capitalize() } ?: println("I can't do that without crashing - beverage was null!")
+    // type of drink, drink name, price
+    placeOrder("shandy,Dragon's Breath,5.91") // no space between
 
-//    if (beverage != null) {
-//        beverage = beverage.capitalize()
-//        println(" not null")
-//    } else {
-//        println("I can't do that without crashing - beverage was null!")
-//    }
+    placeOrder("elixir,Shirley's Temple,4.12")
 }
+
+fun placeOrder(menuData : String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their orders.")
+
+    // menu
+//    val data = menuData.split(',')
+//    val type = data[0]
+//    val name = data[1]
+//    val price = data[2]
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price"
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name")}!"
+    } else {
+        "Madrigal says : Thanks for th $name."
+    }
+    println(phrase)
+}
+
+private fun toDragonSpeak(phrase : String) : String =
+    phrase.replace(Regex("[aeiou]")) {
+        when(it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
+
 
